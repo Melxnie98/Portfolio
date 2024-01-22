a367642865dfdda0c42c631b4480bfc65958f190
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-01-22 15:57:49 +1300

    hi

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Melanie Leonard';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Portfolio of work.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
