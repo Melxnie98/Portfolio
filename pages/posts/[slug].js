2ad43ac7361e23857fa1071d0ab8329dc08ea329
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-01-28 12:33:54 +1300

    hi

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -40,6 +40,7 @@ export default function PostPage({
       <SEO
         title={`${frontMatter.title} - ${globalData.name}`}
         description={frontMatter.description}
+        
       />
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
