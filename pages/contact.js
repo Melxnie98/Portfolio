79a7406af310448845f75201cfc05c05a0d779e4
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-01-28 13:24:41 +1300

    big change

---
+++ pages/contact.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import ScrollableHeader from '@components/ScrollableHeader';
+import ContactForm from '../components/ContactForm';
+import styles from '../components/ContactForm.module.css';
+import { useRouter } from 'next/router';
+
+const ContactPage = () => {
+  const router = useRouter();
+  
+    const navigateTo = (path) => {
+      router.push(path);
+    }
+  return (
+    <div >
+      <Header>
+        <title>Contact me</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Header>
+
+      <main>
+     
+        <ContactForm  />
+      
+
+      
+     
+      </main>
+     
+    </div>
+  );
+};
+
+export default ContactPage;
