ecd69745dac835bd7f72d2b76706b126cad06c17
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-01-28 14:10:22 +1300

    huh?

--- pages/contact.js
+++ pages/contact.js
@@ -23,7 +23,7 @@ const ContactPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Header>
 
-      <main>
+      <main className="w-full">
         <ContactForm  />
       </main>
      
