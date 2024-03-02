19dbff9f756d6f30cdb60783359a78e6a8a9cab1
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-03-02 16:23:17 +1300

    experience

--- pages/experiance.js
+++ pages/experiance.js
@@ -37,15 +37,18 @@ export default function ExperiancePage({ posts, globalData }) {
         <p>
           As a Freelance Web Designer/Developer, I specialised in crafting responsive and visually appealing websites that prioritize usability and accessibility. Leveraging my expertise in Node.js, CSS, and HTML, I designed and implemented custom solutions tailored to meet clients' specific needs. With a keen eye for detail and a focus on optimization for speed and SEO, I delivered high-quality websites that elevated brands and enhanced user experiences.
         </p>
+        <br></br>
 
         <h2>Laboratory Demonstrator, University College Cork (2023)</h2>
         <p>
           At University College Cork, I served as a Laboratory Demonstrator, guiding students in various aspects of audio and sound engineering, web development for digital media, 3D modelling and Animation and authoring. I mentored students on recording music and voiceovers, provided tutorials on web development technologies, and facilitated workshops on Java programming and application development. Additionally, I assisted in teaching short film creation and production, fostering creativity and technical skills among students.
         </p>
+        <br></br>
         <h2>Priority Operations Specialist, Telus International - Stripe (2022 - 2023)</h2>
         <p>
           As a Priority Operations Specialist at Telus International - Stripe, I began as a customer support specialist before swiftly transitioning to handle more technical and critical issues. I provided support for Stripe products, including API support and resolution of disputes, account issues, and suspicious activity claims. My role involved delivering exceptional customer service via chat, email, and phone, ensuring timely resolution of issues to maintain customer satisfaction.
         </p>
+        <br></br>
         <h2>Customer Support Representative, SAP Galway (2021 - 2022)</h2>
         <p>
           During my tenure at SAP Galway, as part of the iXP intern program, I offered customer support services to clients worldwide, with a focus on Spanish and English-speaking customers. I actively participated in SAP's Dev X Challenge, contributing to a team that won first prize for developing innovative solutions to enhance mobility in Dublin. My role involved resolving customer inquiries, providing technical assistance, and fostering positive customer relationships.
