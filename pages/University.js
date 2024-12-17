3c90935edd13b9b8be75a133b30ec529f581a6be
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-12-17 15:02:08 +1300

    contentBlock css

--- pages/University.js
+++ pages/University.js
@@ -32,32 +32,6 @@ export default function UniversityPage({ posts, globalData }) {
       <h1 className="text-3xl lg:text-5xl text-center mb-12">
         ⭐Education⭐
         </h1>
-        <h2>University of Limerick</h2>
-        <p>
-            My educational journey has been a rich tapestry of linguistic exploration, cultural immersion, and technological innovation. I completed my Bachelor of Arts with a focus on languages and linguistics, specializing in Teaching English to Speakers of Other Languages (TESOL) while also delving into Spanish and Japanese language and culture. During my undergraduate studies, I undertook a significant translation project, translating Ciro Alegría’s "Fábulas y Leyendas de Latinoamérica" into English, which deepened my appreciation for language and literature. This project not only sharpened my language skills but also instilled in me a profound understanding of cross-cultural communication and storytelling.
-        </p>
-        <br></br>
-        <p>
-            My academic pursuits extended beyond linguistics into the realms of sociology, philosophy, and new media studies, providing me with a holistic understanding of the interconnectedness of language, society, and technology. This interdisciplinary approach fueled my curiosity and laid the groundwork for my future endeavors in the realm of interactive media.
-        </p>
-        <p>
-        Key Modules:
-        <ul>
-            <li>Translation of Ciro Alegría’s "Fábulas y Leyendas de Latinoamérica"</li>
-            <li>Japanese Language, Culture, and Society (Levels 1-3)</li>
-            <li>Spanish Language and Society (Levels 1-3)</li>
-            <li>Linguistics (Levels 1-6)</li>
-            <li>Celtic Civilization: Culture, Language, and Representations</li>
-            <li>Discourse Analysis for Arts, Humanities & Social Sciences</li>
-            <li>Socio-political Issues in the Hispanic World: Society, Culture, and Representation</li>
-            <li>Modern Trends in Hispanic Culture and the Arts</li>
-            <li>Indigenismo and Negrismo in Latin American Literature</li>
-            <li>Modern and Contemporary Spain</li>
-            <li>Media and Current Issues in the Spanish-speaking World</li>
-            <li>Women's Narratives of Resistance in the Hispanic World Spain, Europe, and Beyond</li>
-        </ul>
-        </p>
-        <br></br>
         <h2>University College Cork</h2>
         <p>
             Building upon this foundation, I pursued a Master's degree in Interactive Media, where I delved into the cutting-edge realms of digital design and technology. Here, I had the opportunity to engage in a diverse range of projects that pushed the boundaries of innovation and creativity. One of the highlights of my master's program was my thesis project, which involved the development, implementation, and evaluation of a groundbreaking VR tool for empathy training in student Intellectual Disability Nurses. This project incorporated elements such as 360-degree video, ambisonic sound, and haptic feedback, providing a immersive and impactful learning experience. The success of this project not only demonstrated my technical proficiency but also underscored my passion for using technology for social good.
@@ -83,6 +57,33 @@ export default function UniversityPage({ posts, globalData }) {
                 <li>Audio Processing</li>
             </ul>
         </p>
+        <div class="contentBlock">
+        <h2>University of Limerick</h2>
+        <p>
+            My educational journey has been a rich tapestry of linguistic exploration, cultural immersion, and technological innovation. I completed my Bachelor of Arts with a focus on languages and linguistics, specializing in Teaching English to Speakers of Other Languages (TESOL) while also delving into Spanish and Japanese language and culture. During my undergraduate studies, I undertook a significant translation project, translating Ciro Alegría’s "Fábulas y Leyendas de Latinoamérica" into English, which deepened my appreciation for language and literature. This project not only sharpened my language skills but also instilled in me a profound understanding of cross-cultural communication and storytelling.
+        </p>
+        <br></br>
+        <p>
+            My academic pursuits extended beyond linguistics into the realms of sociology, philosophy, and new media studies, providing me with a holistic understanding of the interconnectedness of language, society, and technology. This interdisciplinary approach fueled my curiosity and laid the groundwork for my future endeavors in the realm of interactive media.
+        </p>
+        <p>
+        Key Modules:
+        <ul>
+            <li>Translation of Ciro Alegría’s "Fábulas y Leyendas de Latinoamérica"</li>
+            <li>Japanese Language, Culture, and Society (Levels 1-3)</li>
+            <li>Spanish Language and Society (Levels 1-3)</li>
+            <li>Linguistics (Levels 1-6)</li>
+            <li>Celtic Civilization: Culture, Language, and Representations</li>
+            <li>Discourse Analysis for Arts, Humanities & Social Sciences</li>
+            <li>Socio-political Issues in the Hispanic World: Society, Culture, and Representation</li>
+            <li>Modern Trends in Hispanic Culture and the Arts</li>
+            <li>Indigenismo and Negrismo in Latin American Literature</li>
+            <li>Modern and Contemporary Spain</li>
+            <li>Media and Current Issues in the Spanish-speaking World</li>
+            <li>Women's Narratives of Resistance in the Hispanic World Spain, Europe, and Beyond</li>
+        </ul>
+        </div>
+        
       </main>
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
